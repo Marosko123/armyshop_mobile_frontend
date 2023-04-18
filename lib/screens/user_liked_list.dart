0b6f50822da3d8627bef0d3b13b3f921435cccdc
Michal Darovec <michal.darovec@gmail.com>
2023-04-18 09:07:49 +0200

    liked list changes

--- lib/screens/user_liked_list.dart
+++ lib/screens/user_liked_list.dart
@@ -39,14 +39,14 @@ class UserLikedListState extends State<UserLikedList> {
               mainAxisSpacing: 15.0,
               childAspectRatio: 0.8,
               children: <Widget>[
-                _buildCard('AK 47', '\$3.99',
-                    'assets/images/army-bg1.jpg', context),
-                _buildCard('AK 47', '\$5.99',
-                    'assets/images/army-bg1.jpg', context),
-                _buildCard('AK 47', '\$1.99',
-                    'assets/images/army-bg1.jpg', context),
-                _buildCard('AK 47', '\$2.99',
-                    'assets/images/army-bg1.jpg', context)
+                _buildCard(
+                    'AK 47', '\$3.99', 'assets/images/army-bg1.jpg', context),
+                _buildCard(
+                    'AK 47', '\$5.99', 'assets/images/army-bg1.jpg', context),
+                _buildCard(
+                    'AK 47', '\$1.99', 'assets/images/army-bg1.jpg', context),
+                _buildCard(
+                    'AK 47', '\$2.99', 'assets/images/army-bg1.jpg', context)
               ],
             ),
           ),
@@ -58,8 +58,20 @@ class UserLikedListState extends State<UserLikedList> {
 
   Widget _buildCard(
       String name, String price, String image, BuildContext context) {
+    // set the height of the card
+    double deviceWidth = MediaQuery.of(context).size.width;
+    double cardHeight;
+    if (deviceWidth < 600) {
+      cardHeight = MediaQuery.of(context).size.height * 0.16;
+    } else if (deviceWidth < 1000) {
+      cardHeight = MediaQuery.of(context).size.height * 0.24;
+    } else {
+      cardHeight = MediaQuery.of(context).size.height * 0.40;
+    }
+
     return Padding(
-      padding: EdgeInsets.only(top: 5.0, bottom: 5.0, left: 5.0, right: 5.0),
+      padding:
+          const EdgeInsets.only(top: 5.0, bottom: 5.0, left: 5.0, right: 5.0),
       child: InkWell(
         onTap: () {},
         child: Container(
@@ -70,7 +82,7 @@ class UserLikedListState extends State<UserLikedList> {
                 color: Colors.grey.withOpacity(0.2),
                 spreadRadius: 3,
                 blurRadius: 5,
-                offset: Offset(0, 3), // changes position of shadow
+                offset: const Offset(0, 3), // changes position of shadow
               ),
             ],
             color: Colors.white,
@@ -79,7 +91,7 @@ class UserLikedListState extends State<UserLikedList> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               Container(
-                height: MediaQuery.of(context).size.height * 0.20,
+                height: cardHeight,
                 width: double.infinity,
                 child: Hero(
                   tag: image,
@@ -96,34 +108,59 @@ class UserLikedListState extends State<UserLikedList> {
                   ),
                 ),
               ),
-              SizedBox(height: 2.0),
-              Text(
-                name,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16.0,
-                ),
+              const SizedBox(height: 2.0),
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.spaceBetween, // Space between columns
+                crossAxisAlignment:
+                    CrossAxisAlignment.center, // Center vertically
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name,
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        price,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 10), // Add some space between columns
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                        size: 50,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              SizedBox(height: 2.0),
-              Text(
-                price,
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 14.0,
-                ),
-              ),
-              SizedBox(height: 2.0),
+              const SizedBox(height: 2.0),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Icon(
                     Icons.favorite,
                     color: Colors.red,
+                    size: 30,
                   ),
                   Icon(
                     Icons.shopping_basket,
                     color: Theme.of(context).primaryColor,
+                    size: 30,
                   ),
                 ],
               )
